2aec926d6b3ec2972f73400fb398107f2074e189
rohang <rohan.gunge@vtinfo.com>
2025-07-10 05:58:03 +0000

    OS_62

--- db/migrate/20250707104310_add_phone_number_to_users.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# This migration adds a phone_number column to the users table.
-# The column is of type string and can store users' contact numbers.
-class AddPhoneNumberToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :phone_number, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,19 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 20_250_707_104_310) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_10_050640) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'pg_catalog.plpgsql'
+  enable_extension "pg_catalog.plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'email'
-    t.integer 'age'
-    t.date 'date_of_birth'
-    t.string 'password'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'phone_number'
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "age"
+    t.date "date_of_birth"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
