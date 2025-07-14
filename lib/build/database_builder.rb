d3039c7781a790a8a91f8aa9664d92dcad589f3c
rohang0110 <rohan.gunge@vtinfo.com>
2025-07-14 18:20:27 +0530

    Update database_builder.rb

--- lib/build/database_builder.rb
+++ lib/build/database_builder.rb
@@ -1,12 +1,21 @@
 # lib/build/database_builder.rb
 
+require 'faker'
+
 module Build
   class DatabaseBuilder
+    def reset_data
+      puts "Resetting database..."
+      User.destroy_all
+      puts "All users destroyed."
+    end
+
     def create_users
       puts "Creating users using Faker"
 
       10.times do
         date_of_birth = Faker::Date.birthday(min_age: 18, max_age: 100)
+        age = ((Time.zone.now - date_of_birth.to_time) / 1.year.seconds).floor
         password = Faker::Internet.password(min_length: 6)
 
         User.create!(
@@ -15,12 +24,23 @@ module Build
           email: Faker::Internet.unique.email,
           phone_number: Faker::Number.number(digits: 10).to_s.rjust(10, '0'),
           date_of_birth: date_of_birth,
+          age: age, # Pass the calculated age
           password: password,
           password_confirmation: password
         )
       end
 
       puts "10 users with Faker data created successfully!"
+    end
+
+
+    def execute
+      reset_data
+      create_users
+    end
+
+    def run
+      execute
     end
   end
 end
