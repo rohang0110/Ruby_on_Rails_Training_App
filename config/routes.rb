b72b23509f35ab71a2103643db24eb568d720a82
rohang <rohan.gunge@vtinfo.com>
2025-07-17 12:41:27 +0000

    OS 69

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[index create show update]
+      resources :users, only: %i[index create show update destroy]
     end
   end
   # Defines the root path route ("/")
