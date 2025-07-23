4e151bd3643e677bdc5aec3d7d61e7cdb58b5f5f
rohang <rohan.gunge@vtinfo.com>
2025-07-23 08:53:26 +0000

    added rspec test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,7 +16,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-
+  config.include Devise::Test::IntegrationHelpers, type: :request
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
