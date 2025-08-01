23b3f3b765d118bc0f8c5fcc5673d10110b2963e
rohang <rohan.gunge@vtinfo.com>
2025-08-01 09:34:50 +0000

    sign in changes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,15 +8,19 @@ class Users::SessionsController < Devise::SessionsController
     return unless params[:user].present?
 
     user = User.find_by(email: params[:user][:email].downcase)
-    return unless user && user.valid_password?(params[:user][:password]) # Ensure password matches
+    return unless user&.valid_password?(params[:user][:password]) # Ensure password is valid
 
-    # Fallback to params[:role], which should be passed via ?role=staff or ?role=customer
     role_param = params[:role]
 
+    if role_param.blank?
+      flash[:alert] = 'You are not authorized to sign in from this route.'
+      redirect_to root_path and return
+    end
+
     if role_param == 'staff' && !user.staff?
-      redirect_to new_user_session_path(role: 'staff'), alert: 'Only staff can log in here.' and return
+      redirect_to new_staff_session_path, alert: 'Only staff can log in here.' and return
     elsif role_param == 'customer' && !user.customer?
-      redirect_to new_user_session_path(role: 'customer'), alert: 'Only customers can log in here.' and return
+      redirect_to new_customer_session_path, alert: 'Only customers can log in here.' and return
     end
   end
 end
